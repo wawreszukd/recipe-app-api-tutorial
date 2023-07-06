35a94b9a971ecfd0211f76d9d512b78065b2b20d
wawreszukd <wawreszukd@gmail.com>
2023-07-06 16:08:40 +0200

    Changing django admin:

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS')
-   }
+    }
 }
 
 
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (_('Login data'), {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Importnat dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (_('Login data'),
+         {
+             'classes': ('wide',),
+             'fields': (
+                 'email',
+                 'password',
+                 'password2',
+                 'name',
+                 'is_active',
+                 'is_staff',
+                 'is_superuser',
+             )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
